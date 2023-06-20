78cf031bc95b1bcf004d913837eac3cd4ef79937
yaminiu <yniu@live.com.au>
2023-06-20 21:58:16 +1000

    Add to gihthub

---
+++ pokemon.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+from mlforkids import MLforKidsImageProject
+
+# treat this key like a password and keep it secret!
+key = "718280d0-0f5c-11ee-8bb1-1f569cdc22a4a5f7362d-0020-41aa-ae1f-b533f100ad79"
+
+# this will train your model and might take a little while
+myproject = MLforKidsImageProject(key)
+myproject.train_model()
+
+# CHANGE THIS to the image file you want to recognize
+demo = myproject.prediction("/mnt/c/Users/ethan/OneDrive/Desktop/Ethandoc/icode/pokemon1.png")
+
+label = demo["class_name"]
+confidence = demo["confidence"]
+
+# CHANGE THIS to do something different with the result
+print ("result: '%s' with %d%% confidence" % (label, confidence))
